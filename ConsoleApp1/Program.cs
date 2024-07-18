e4ba0f8b70e684a6d985d5894e4e96456607a574
stasbasi <stasbasi@STASBASI>
2024-07-18 23:16:28 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace myFirstCode
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sura Tasbasi");
+            Console.WriteLine("Patika'nin uzman kadrosu ve tecrübeli egitmenleri sayesinde yazilim dunyasina hizli bir sekilde giris yapabilecegim icin bu bootcampteyim!");
+
+        }
+
+    }
+}
